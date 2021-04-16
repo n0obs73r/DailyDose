45e1fbd1e25372801d713146aa704005b38638e9
Aryan <aryan.18bcon297@jecrcu.edu.in>
2021-04-16 19:20:24 +0530

    Initial Commit

--- app/src/main/java/com/example/mychittor/SignupActivity.kt
+++ app/src/main/java/com/example/mychittor/SignupActivity.kt
@@ -1,13 +1,31 @@
 package com.example.mychittor
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.auth.FirebaseAuth
 
+private lateinit var auth: FirebaseAuth
 class SignupActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup)
 
 
+        auth = FirebaseAuth.getInstance()
+
+        auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(this, OnCompleteListener{ task ->
+            if(task.isSuccessful){
+                Toast.makeText(this, "Successfully Registered", Toast.LENGTH_LONG).show()
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+                finish()
+            }else {
+                Toast.makeText(this, "Registration Failed", Toast.LENGTH_LONG).show()
+            }
+        })
     }
 }
