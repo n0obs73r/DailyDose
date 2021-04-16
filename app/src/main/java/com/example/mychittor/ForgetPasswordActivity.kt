d4c0fa733a4ab0a876299c459e502cc0e3dfcb34
Aryan <aryan.18bcon297@jecrcu.edu.in>
2021-04-16 18:53:06 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/mychittor/ForgetPasswordActivity.kt
@@ -0,0 +1,22 @@
+
+
+
+
+
+
+
+
+
+
+
+package com.example.mychittor
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ForgetPasswordActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_forget_password)
+    }
+}
---
+++ app/src/main/java/com/example/mychittor/SignupActivity.kt
@@ -0,0 +1,13 @@
+package com.example.mychittor
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SignupActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signup)
+
+
+    }
+}
