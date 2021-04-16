c0311cfa1f4a2316cc31e7ae8659c56b93cd30fd
Aryan <aryan.18bcon297@jecrcu.edu.in>
2021-04-16 19:36:07 +0530

    Initial Commit

--- app/src/main/java/com/example/mychittor/SignupActivity.kt
+++ app/src/main/java/com/example/mychittor/SignupActivity.kt
@@ -3,21 +3,28 @@ package com.example.mychittor
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.widget.EditText
 import android.widget.Toast
 import com.google.android.gms.tasks.OnCompleteListener
 import com.google.firebase.auth.FirebaseAuth
 
-private lateinit var auth: FirebaseAuth
 class SignupActivity : AppCompatActivity() {
 
+    private lateinit var auth: FirebaseAuth
+    private lateinit var editText: EditText
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup)
 
+        var userText : EditText = findViewById(R.id.InputEmail)
+        var passText : EditText = findViewById(R.id.InputPassword)
+        val email : Editable? = userText.getText()
+        val password : Editable? = passText.getText()
 
         auth = FirebaseAuth.getInstance()
 
-        auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(this, OnCompleteListener{ task ->
+        auth.createUserWithEmailAndPassword(email.toString(), password.toString()).addOnCompleteListener(this, OnCompleteListener{ task ->
             if(task.isSuccessful){
                 Toast.makeText(this, "Successfully Registered", Toast.LENGTH_LONG).show()
                 val intent = Intent(this, MainActivity::class.java)
