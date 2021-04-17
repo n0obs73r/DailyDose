7964acf7fd4fcf624ae23cc705e01f3001763b87
Aryan <aryan.18bcon297@jecrcu.edu.in>
2021-04-17 13:09:38 +0530

    News api fetching done and dusted

---
+++ app/src/main/java/com/example/mychittor/main/ui/ItemAdapter.kt
@@ -0,0 +1,59 @@
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.Button
+import android.view.ViewGroup
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.core.content.ContextCompat.startActivity
+import androidx.recyclerview.widget.RecyclerView
+import com.example.mychittor.NewsData
+import com.example.mychittor.R
+
+
+class ItemAdapter(context: Context, items: ArrayList<NewsData>) :
+    RecyclerView.Adapter<ItemAdapter.ViewHolder>() {
+    var context: Context
+    private val items: ArrayList<NewsData>
+    override fun onCreateViewHolder(viewGroup: ViewGroup, i: Int): ViewHolder {
+        val inflater = LayoutInflater.from(context)
+        val layout = inflater.inflate(R.layout.news_items, null) as LinearLayout
+        return ViewHolder(layout)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, i: Int) {
+        val item: NewsData = items[i]
+        val title: String? = item.mTitle
+        val url: String? = item.mWebURL
+        holder.editTitle.text = title
+        holder.mybutton.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(v: View?) {
+
+                val uri : Uri = Uri.parse(url)
+                context.startActivity(Intent(Intent.ACTION_VIEW, uri))
+
+            }
+        })
+    }
+
+    override fun getItemCount(): Int {
+        return items.size
+    }
+
+     inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        var editTitle: TextView
+        var mybutton: Button
+
+        init {
+            editTitle = itemView.findViewById(R.id.tittle1)
+            mybutton = itemView.findViewById(R.id.details)
+        }
+    }
+
+    init {
+        this.context = context
+        this.items = items
+    }
+}
--- app/src/main/java/com/example/mychittor/main/ui/News.kt
+++ app/src/main/java/com/example/mychittor/main/ui/News.kt
@@ -10,6 +10,8 @@ class News : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_news, container, false)
     }
